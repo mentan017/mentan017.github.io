bc99955f8ea71a4ad4c836c585e3d8193d7c2007
Adrian Bernabeu <mentan017@pop-os.localdomain>
2023-01-20 22:31:28 +0100

    Modifications to Sophie Schenk

--- Sophie_Schenk/script.js
+++ Sophie_Schenk/script.js
@@ -1,3 +1,7 @@
+document.onload = ChangeImgs();
+
+var CurrentAboutImg = 0;
+
 document.getElementById('more-btn').addEventListener('click', function(e){
     e.stopImmediatePropagation();
     document.getElementById('about-me').scrollIntoView({behavior: 'smooth'});
@@ -6,3 +10,10 @@ document.getElementById('skills-btn').addEventListener('click', function(e){
     e.stopImmediatePropagation();
     document.getElementById('skills').scrollIntoView({behavior: 'smooth'});
 });
+
+function ChangeImgs(){
+    var AboutImgs = ['About_Me_0.png', 'About_Me_1.png', 'About_Me_2.png'];
+    if(CurrentAboutImg != undefined) document.getElementById("about-me-img").setAttribute('src', `./Images/${AboutImgs[CurrentAboutImg]}`);
+    CurrentAboutImg = (CurrentAboutImg+1)%3;
+    setTimeout(ChangeImgs, 5000);
+}
